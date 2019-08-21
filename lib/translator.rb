e2ad0d0acea8907c12fe534c4051785448662ac0
Martin Milius <martin.milius@gmail.com>
2019-08-21 14:45:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,17 @@
-# require modules here
+require 'yaml'
 
-def load_library
+def load_library(emoticon_file)
+  emojis = YAML.load_file(emoticon_file)
+  dictionary = {"get_meaning" => {}, "get_emoticon" => {}}
+end
+
+def get_japanese_emoticon(western)
+  load_library
   # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
+def get_english_meaning(japanese)
+  load_library
+  return ENGLISH 
   # code goes here
 end
